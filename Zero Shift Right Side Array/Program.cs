05c84e77556801fe02c1cc84dfdaa9bbf5a6984c
Badruzzaman <badru.cse@gmail.com>
2023-02-11 19:29:01 +0600

    g

---
+++ Zero Shift Right Side Array/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int[] array = { 0, 4, 0, 5, 6, 0, 0 };
+int toMove = 0;
+int left = 0;
+int right = array.Length - 1;
+
+while (left < right)
+{
+    while (left < right && array[right] == toMove)
+    {
+        right--;
+    }
+    if (array[left] == toMove)
+    {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    left++;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.ReadKey();
