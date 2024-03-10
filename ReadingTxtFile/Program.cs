9ab0661e327fafffcfd48e40e578f6da2c36653c
Badruzzaman <badru.cse@gmail.com>
2024-03-10 22:22:12 +0600

    file reading from txt

---
+++ ReadingTxtFile/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+string line = "2023-03-01 08:15:22 INFO User login successful";
+string[] words = line.Split(' ');
+var logFile = new LogFile
+{
+    DateTime = Convert.ToDateTime(words[0] + " " + words[1]),
+    Status = words[2],
+    Message = string.Join(" ", words.Skip(3))
+};
+
+class LogFile
+{
+    internal DateTime DateTime { get; set; }
+    internal string Status { get; set; }
+    internal string Message { get; set; }
+}
